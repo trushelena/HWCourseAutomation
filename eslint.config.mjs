245690c2a7244cd70029f82c650575cb5f4649a1
trushelena <trushelena1996@gmail.com>
2025-01-30 11:29:09 +0100

    esling changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,52 +1,51 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import eslint from '@eslint/js';
+import tsEslint from 'typescript-eslint';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-    {
-        languageOptions: { globals: globals.browser }
+export default tsEslint.config({
+    files: ['**/*.ts'],
+    plugins: {
+        '@stylistic': stylistic,
+        unicorn: eslintPluginUnicorn
     },
-
-    pluginJs.configs.recommended,
-    {
-        plugins: {
-            unicorn: eslintPluginUnicorn
-        },
-        name: 'best practices rules',
-        files: ['**/*.js'],
-        rules: {
-            ...pluginJs.configs.recommended.rules,
-            semi: ['error', 'always'],
-            indent: [
-                'error',
-                4,
-                {
-                    SwitchCase: 1
-                }
-            ],
-            'max-len': [
-                'warn',
-                {
-                    code: 140,
-                    ignoreUrls: true,
-                    ignoreStrings: true,
-                    ignoreTemplateLiterals: true
-                }
-            ],
-            'brace-style': ['warn'],
-            'comma-dangle': ['error', 'never'],
-            'no-multiple-empty-lines': ['error', { max: 2 }],
-            'no-trailing-spaces': ['error', { skipBlankLines: false }],
-            'eol-last': ['error', 'always'],
-            'no-var': ['error'],
-            'unicorn/filename-case': [
-                'error',
-                {
-                    case: 'kebabCase'
-                }
-            ]
-        }
+    extends: [eslint.configs.recommended, ...tsEslint.configs.recommended, ...tsEslint.configs.stylistic],
+    rules: {
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/no-unused-expressions': ['off'],
+        '@typescript-eslint/consistent-indexed-object-style': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/explicit-member-accessibility': 'error',
+        '@typescript-eslint/no-shadow': 'error',
+        '@typescript-eslint/explicit-function-return-type': [
+            'error',
+            {
+                allowExpressions: true
+            }
+        ],
+        '@stylistic/indent': ['error', 4],
+        '@stylistic/max-len': [
+            'warn',
+            {
+                code: 140,
+                ignoreUrls: true,
+                ignoreStrings: true,
+                ignoreTemplateLiterals: true
+            }
+        ],
+        '@stylistic/brace-style': ['warn'],
+        '@stylistic/operator-linebreak': ['warn', 'after', { overrides: { '?': 'before', ':': 'before' } }],
+        '@stylistic/quotes': ['warn', 'single'],
+        '@stylistic/semi': ['error', 'always'],
+        '@stylistic/comma-dangle': ['error', 'never'],
+        '@stylistic/no-multiple-empty-lines': ['error', { max: 2 }],
+        '@stylistic/no-trailing-spaces': ['error', { skipBlankLines: false }],
+        '@stylistic/eol-last': ['error', 'always'],
+        'unicorn/filename-case': [
+            'error',
+            {
+                case: 'kebabCase'
+            }
+        ]
     }
-];
+});
