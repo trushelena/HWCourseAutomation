d2f93d4c423e60db9001b750c7d3458771f57c66
trushelena <trushelena1996@gmail.com>
2025-02-02 18:32:57 +0100

    Conflicts fix v2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,56 +1,12 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    {
-        languageOptions: { globals: globals.browser }
-    },
-
-    pluginJs.configs.recommended,
-    {
-        plugins: {
-            unicorn: eslintPluginUnicorn
-        },
-        name: 'best practices rules',
-        files: ['**/*.js'],
-        rules: {
-            ...pluginJs.configs.recommended.rules,
-            semi: ['error', 'always'],
-            indent: [
-                'error',
-                4,
-                {
-                    SwitchCase: 1
-                }
-            ],
-            'max-len': [
-                'warn',
-                {
-                    code: 140,
-                    ignoreUrls: true,
-                    ignoreStrings: true,
-                    ignoreTemplateLiterals: true
-                }
-            ],
-            'brace-style': ['warn'],
-            'comma-dangle': ['error', 'never'],
-            'no-multiple-empty-lines': ['error', { max: 2 }],
-            'no-trailing-spaces': ['error', { skipBlankLines: false }],
-            'eol-last': ['error', 'always'],
-            'no-var': ['error'],
-<<<<<<< HEAD
-=======
-            'prefer-const': 'error',
->>>>>>> df4738d6133ff2cf67c79dfb0daf17f30d8ca788
-            'unicorn/filename-case': [
-                'error',
-                {
-                    case: 'kebabCase'
-                }
-            ]
-        }
-    }
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
