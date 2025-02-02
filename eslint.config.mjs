db48deaf21b0f67ec8a4846970d55744ac12e30a
trushelena <trushelena1996@gmail.com>
2025-02-02 18:45:37 +0100

    eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,53 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    {
+        languageOptions: { globals: globals.browser }
+    },
+
+    pluginJs.configs.recommended,
+    {
+        plugins: {
+            unicorn: eslintPluginUnicorn
+        },
+        name: 'best practices rules',
+        files: ['**/*.js'],
+        rules: {
+            ...pluginJs.configs.recommended.rules,
+            semi: ['error', 'always'],
+            indent: [
+                'error',
+                4,
+                {
+                    SwitchCase: 1
+                }
+            ],
+            'max-len': [
+                'warn',
+                {
+                    code: 140,
+                    ignoreUrls: true,
+                    ignoreStrings: true,
+                    ignoreTemplateLiterals: true
+                }
+            ],
+            'brace-style': ['warn'],
+            'comma-dangle': ['error', 'never'],
+            'no-multiple-empty-lines': ['error', { max: 2 }],
+            'no-trailing-spaces': ['error', { skipBlankLines: false }],
+            'eol-last': ['error', 'always'],
+            'no-var': ['error'],
+            'prefer-const': 'error',
+            'unicorn/filename-case': [
+                'error',
+                {
+                    case: 'kebabCase'
+                }
+            ]
+        }
+    }
 ];
