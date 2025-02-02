38622741f2c5ab08f5b93206eb7ff5560377efd5
trushelena <trushelena1996@gmail.com>
2025-02-02 11:22:28 +0100

    Adding file loop.js

---
+++ loop.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i); // Show value for each iteration
+};
+
+
+for (let i = 100; i >=0; i-=10) {
+    console.log(i); // Show value for each iteration
+};
