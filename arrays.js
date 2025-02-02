2ec816bd974e7e3c85a2c479a1f4e2348a972371
trushelena <trushelena1996@gmail.com>
2025-02-02 12:43:20 +0100

    Add arrays

---
+++ arrays.js
@@ -0,0 +1,6 @@
+const stringArray = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+console.log("Перебір масиву рядків:");
+stringArray.forEach(item => {
+    console.log(item);  // Виводить кожен елемент масиву
+});
