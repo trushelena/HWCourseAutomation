6849dfc5b6ae6b0e0e38885ce18caef5aee62210
trushelena <trushelena1996@gmail.com>
2025-02-13 17:43:40 +0100

    Fix comments

--- lesson7/src/arrow-functions.ts
+++ lesson7/src/arrow-functions.ts
@@ -1,9 +1,14 @@
-const sumArrayElements = (arr : number []) : number => arr.reduce((sum, current) => sum + current, 0);
+const sumArrayElements = (arr: string[] | number[]): number => {
+    return arr
+        .map(item => typeof item === 'string' ? Number(item) : item)
+        .filter(num => !isNaN(num))
+        .reduce((sum, current) => sum + current, 0);
+};
 
-const numberArray : number [] = [1, 2, 3, 4, 5];
-const stringArray : string [] = ['6', '7', '8', '9', '0'];
+const numberArray = [1, 2, 3, 4, 5];
+const stringArray = ['6', '7', '8', 'er', '0'];
 
-const convertedStringArray : number [] = stringArray.map(Number);
+const convertedStringArray = stringArray.map(Number);
 
 const sumNumbers : number = sumArrayElements(numberArray);
 const sumStrings : number = sumArrayElements(convertedStringArray);
--- lesson7/src/functions.ts
+++ lesson7/src/functions.ts
@@ -1,19 +1,24 @@
-function sumArrayElements(arr : number[]) : number {
+function sumArrayElements(arr: number[] | string[]): number {
     let sum = 0;
     for (const num of arr) {
-        sum += num;
+        sum += typeof num === 'number' ? num : Number(num);
     }
     return sum;
 }
 
+const numberArray = [1, 2, 3, 4, 5];
+const stringArray = ['rjefnjne', '2', 'derdf', '5', 'ref'];
 
-const numberArray : number [] = [1, 2, 3, 4, 5];
-const stringArray : string [] = ['6', '7', '8', '9', '0'];
+function isValidNumber(num: number): boolean {
+    return !Number.isNaN(num);
+}
 
-const convertedStringArray : number [] = stringArray.map(Number);
+const convertedStringArray: number[] = stringArray
+    .map(Number)
+    .filter(isValidNumber);
 
-const sumNumbers : number = sumArrayElements(numberArray);
-const sumStrings : number = sumArrayElements(convertedStringArray);
+const sumNumbers: number = sumArrayElements(numberArray);
+const sumStrings: number = sumArrayElements(convertedStringArray);
 
 console.log('Sum of array numbers:', sumNumbers);
 console.log('Sum of string array:', sumStrings);
