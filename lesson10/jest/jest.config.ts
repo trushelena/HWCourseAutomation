2693825b2f72cb9f242ee736468be2fabe34d261
trushelena <trushelena1996@gmail.com>
2025-02-25 10:21:36 +0100

    jest + performanceLeaderDuties

--- lesson10/jest/jest.config.ts
+++ lesson10/jest/jest.config.ts
@@ -1,18 +1,7 @@
-import type { JestConfigWithTsJest } from 'ts-jest';
-
-const config: JestConfigWithTsJest = {
-    extensionsToTreatAsEsm: ['.ts'],
-    verbose: true,
-    preset: 'ts-jest/presets/default-esm',
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
-    },
-    testPathIgnorePatterns: ['./dist'],
-    clearMocks: true,
-    coverageProvider: 'v8',
-    testMatch: ['**/tests/**/*.[t]s?(x)', '**/?(*.)+(spec|test).[t]s?(x)'],
-    globalSetup: './src/jest-global-setup.ts'
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1'
+  }
 };
-
-export default config;
--- lesson10/jest/tests/jest-first-test.spec.ts
+++ lesson10/jest/tests/jest-first-test.spec.ts
@@ -1,36 +1,26 @@
-import { expect as expectChai } from 'chai';
-import { beforeAll, expect } from '@jest/globals';
+import { Employee } from 'src/employee';
+import { LeadDeveloper } from 'src/lead-developer';
+import { ProjectManager } from 'src/project-manager';
 
-describe('Jest first test suite', () => {
-    let sum: number;
-    beforeAll(() => {
-        sum = 5;
-    });
+describe('LeadDeveloper', () => {
+    let leadDev: LeadDeveloper;
+    let employee: Employee;
+    let pm: ProjectManager;
 
     beforeEach(() => {
-        sum = 6;
+        leadDev = new LeadDeveloper('Artur', 80000, 'TypeScript');
+        employee = new Employee('Vlad', 'QA', 50000);
+        pm = new ProjectManager('Alina', 5430);
     });
 
-    describe('Jest first test', () => {
-        it('2 + 2 = 4', () => {
-            expect(2 + 2).toBe(4);
-        });
-    });
-
-    describe('Jest second test, 2 + 2 = 5', () => {
-        beforeEach(() => {
-            sum = 7;
-        });
-        it('1. put result of 2 + 2 into variable', () => {
-            sum = 2 + 2;
-        });
-
-        it('2. check variable to be 5', () => {
-            expectChai(sum).to.be.equal(5);
-        });
-
-        it('3. final test case', () => {
-            console.log('test');
-        });
+    it('should evaluate performance correctly with Lead Dev position', () => {
+        expect(leadDev.performLeaderDuties(employee)).toBe(
+            'Artur is evaluating the performance Vlad.'
+        );
+    });   
+    it('should evaluate performance correctly with PM position', () => {
+            expect(pm.performLeaderDuties(employee)).toBe(
+                'Alina is evaluating the performance Vlad.'
+        );
     });
 });
