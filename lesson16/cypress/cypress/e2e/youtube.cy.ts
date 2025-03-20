f1c7e59e484a37a46826050131b5644732304e27
trushelena <trushelena1996@gmail.com>
2025-03-20 21:08:58 +0100

    cypress fix

--- lesson16/cypress/cypress/e2e/youtube.cy.ts
+++ lesson16/cypress/cypress/e2e/youtube.cy.ts
@@ -1,45 +1,65 @@
 describe('YouTube Functionality Tests', () => {
     beforeEach(() => {
         cy.visit('https://www.youtube.com/?hl=en');
-        cy.wait(3000);
 
-        cy.get('ytd-button-renderer:nth-child(2) button .yt-spec-touch-feedback-shape__fill').click();
-        cy.wait(3000);
+        cy.get('ytd-button-renderer:nth-child(2) button .yt-spec-touch-feedback-shape__fill')
+            .click();
     });
 
     it('should verify search functionality', () => {
-        cy.wait(5000);
-        cy.get('input[name="search_query"]').should('be.visible');
-        cy.wait(5000);
-        cy.get('input[name="search_query"]').type('Css selectors');
+        cy.get('input[name="search_query"]')
+            .should('be.visible')
+            .type('Css selectors');
 
+        cy.get('.ytSearchboxComponentSearchButton')
+            .should('be.visible')
+            .click();
 
-        cy.get('.ytSearchboxComponentSearchButton').should('be.visible').click();
+        cy.get('#dismissible', { timeout: 60000 })
+            .should('exist')
+            .should('have.length.greaterThan', 0)
+            .then((elements) => {
+                console.log('#dismissible elements are present on the page', elements.length);
+            });
 
-        cy.wait(5000);
-        cy.get('#dismissible', { timeout: 60000 }).should('exist');
+        cy.get('.ytSearchboxComponentClearButton')
+            .should('be.visible')
+            .click();
 
-        cy.get('#dismissible').should('have.length.greaterThan', 0);
-        cy.get('#dismissible').then((elements) => {
-            console.log('#dismissible elements are present on the page', elements.length);
-        });
-
-        cy.wait(3000);
-        cy.get('.ytSearchboxComponentClearButton').should('be.visible').click();
-
-        cy.wait(5000);
         cy.get('input[name="search_query"]').should('have.value', '');
     });
 
-    // Test Case 2: Go from youtube.com to https://music.youtube.com/
-    it('should navigate from YouTube to YouTube Music', () => {
-        cy.wait(5000);
+    describe('Navigation from YouTube to YouTube Music', () => {
 
-        cy.get('#guide-button.ytd-masthead').click();
-        cy.wait(5000);
-        cy.xpath('(//*[@id="endpoint"]/tp-yt-paper-item)[1]').click();
+        it('should navigate from YouTube to YouTube Music', () => {
 
-        cy.get('yt-formatted-string').contains('YouTube Music').click();
+            cy.window().then((win) => {
+                cy.stub(win, 'open').callsFake((url) => {
+                    expect(url).to.include('music.youtube.com');
+                    return null;
+                }).as('popup');
+            });
+
+            cy.get('#guide-button.ytd-masthead', { timeout: 60000 })
+                .should('be.visible')
+                .click();
+
+            cy.xpath('(//*[@id="endpoint"]/tp-yt-paper-item)[1]', { timeout: 120000 })
+                .should('be.visible')
+                .click();
+
+            cy.window().then((win) => {
+                cy.stub(win, 'open').as('popup');
+            });
+
+            cy.contains('yt-formatted-string', 'YouTube Music', { timeout: 60000 })
+                .should('be.visible')
+                .click();
+
+            cy.get('@popup').should('be.called');
+
+            cy.get('@popup').should('have.been.calledOnce');
+        });
     });
-
 });
+
