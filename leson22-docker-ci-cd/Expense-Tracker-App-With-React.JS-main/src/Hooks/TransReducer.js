3a5db2f4c21fbc555907b76a7bdc15ee91a44718
trushelena <trushelena1996@gmail.com>
2025-04-18 17:21:37 +0200

    added githab actions pipeline

--- leson22-docker-ci-cd/Expense-Tracker-App-With-React.JS-main/src/Hooks/TransReducer.js
+++ leson22-docker-ci-cd/Expense-Tracker-App-With-React.JS-main/src/Hooks/TransReducer.js
@@ -1,19 +1,20 @@
-const TransReducer = (state, action) => {
+const transReducer = (state, action) => {
     switch(action.type) {
         case 'DELETE_TRANSACTION':
             return {
                 ...state,
                 transactions: state.transactions
                     .filter(transaction => transaction.id !== action.payload)
-            };
+            }
+            
         case 'ADD_TRANSACTION':
             return {
                 ...state,
                 transactions: [action.payload, ...state.transactions]
-            };
+            }
         default:
             return state;
-    }
+    };
 };
 
-export default TransReducer;
+export default transReducer;
