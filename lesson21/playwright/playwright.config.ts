2348aaba5ca4992922d52248b3d6f825180ee70d
trushelena <trushelena1996@gmail.com>
2025-04-18 19:26:11 +0200

    Add GitHub Actions pipeline for Playwright lesson21 headed + junit report

--- lesson21/playwright/playwright.config.ts
+++ lesson21/playwright/playwright.config.ts
@@ -22,7 +22,12 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [['html'],  ['allure-playwright']],
+    reporter: [
+        ['html', { open: 'never' }],
+        ['allure-playwright'],
+        ['junit', { outputFile: 'test-results/junit.xml' }],
+        ['pwmochawesome', { charts: true }]
+    ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
