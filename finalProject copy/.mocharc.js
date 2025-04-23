6b6faed5c0c57b3848895c5ad5f7fed00c0a0bdc
trushelena <trushelena1996@gmail.com>
2025-04-23 18:05:53 +0200

    copy removed

--- finalProject copy/.mocharc.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    require: ['ts-node/register', 'allure-mocha'],
-    extension: ['ts'],
-    spec: 'tests/api/**/*.spec.ts',
-    reporter: 'allure-mocha',
-    reporterOptions: ['resultsDir=allure-results']
-  };
-  
